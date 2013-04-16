e332b000949ed999860f58230de05ebf6b031b53
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2013-04-16 11:29:53 -0400

    Fill out STOMP callbacks.

--- src/resource_discovery_sup.erl
+++ src/resource_discovery_sup.erl
@@ -11,18 +11,20 @@
 
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(SERVER, ?MODULE).
 
 %% ===================================================================
 %% API functions
 %% ===================================================================
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    RdServer = ?CHILD(rd_server, worker),
+    {ok, { {one_for_one, 5, 10}, [RdServer]} }.
 
