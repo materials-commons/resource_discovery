ef8f69d98ecea05b66d19cb05b9de2b85288f31d
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2013-04-22 09:26:06 -0400

    Add logic in for timeouts, and spawns for pinging other resource hosts.

--- src/rd_ping_server.erl
+++ src/rd_ping_server.erl
@@ -34,23 +34,38 @@ ping_all() ->
 %% ===================================================================
 
 init([LeaseTime]) ->
+    %% Trap exits and handle ourselves.
+    process_flag(trap_exit, true),
     Now = lease:seconds_now(),
     TimeLeft = lease:time_left(Now, LeaseTime),
     {ok, #state{lease_time = LeaseTime, start_time = Now}, TimeLeft}.
 
-handle_call(Msg, _From, State) ->
-    {reply, {ok, Msg}, State}.
+handle_call(_Msg, _From, #state{lease_time = LeaseTime,
+                                start_time = StartTime} = State) ->
+    % Error log unknown message
+    TimeLeft = lease:time_left(StartTime, LeaseTime),
+    {reply, ok, State, TimeLeft}.
 
-handle_cast({ping, _Host}, State) ->
-    {noreply, State};
-handle_cast(ping_all, State) ->
-    {noreply, State};
+handle_cast({ping, Host}, #state{lease_time = LeaseTime,
+                                    start_time = StartTime} = State) ->
+    case rd_store:lookup(Host) of
+        {ok, Pid} -> start_pinger(Host, Pid);
+        {error, not_found} -> ok
+    end,
+    TimeLeft = lease:time_left(StartTime, LeaseTime),
+    {noreply, State, TimeLeft};
+handle_cast(ping_all, #state{lease_time = LeaseTime,
+                                start_time = StartTime} = State) ->
+    start_pingers(),
+    TimeLeft = lease:time_left(StartTime, LeaseTime),
+    {noreply, State, TimeLeft};
 handle_cast(stop, State) ->
     {stop, normal, State}.
 
 handle_info({'EXIT', _Pid, _Reason}, State) ->
     {noreply, State};
 handle_info(timeout, #state{lease_time = LeaseTime} = State)->
+    start_pingers(),
     Now = lease:seconds_now(),
     NewTimeout = lease:time_left(Now, LeaseTime),
     {noreply, State#state{start_time = Now}, NewTimeout}.
@@ -64,4 +79,16 @@ code_change(_OldVsn, State, _Extra) ->
 %% ===================================================================
 %% Local
 %% ===================================================================
+
+start_pingers() ->
+    lists:foreach(fun start_pinger/1, rd_store:all()).
+
+start_pinger({Host, Pid}) ->
+    start_pinger(Host, Pid).
+
+start_pinger(Host, Pid) ->
+    spawn(rd_ping, ping_host, [Host, Pid]).
+
+
+
 
