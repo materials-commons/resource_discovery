76793f3c16155ccbfb202c21e4d2147443c05386
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2013-04-22 11:51:03 -0400

    Pong server functionality.

--- src/rd_pong_server.erl
+++ src/rd_pong_server.erl
@@ -6,37 +6,42 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0]).
+-export([start_link/1]).
 
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
             terminate/2, code_change/3]).
 
--define(SERVER, ?MODULE).
-
--record(state, {}).
+-record(state, {lsock}).
 
 %% ===================================================================
 %% API
 %% ===================================================================
-start_link() ->
-    gen_server:start_link({local, ?SERVER}, [], []).
+start_link(LSock) ->
+    gen_server:start_link(?MODULE, [LSock], []).
 
 %% ===================================================================
 %% gen_server callbacks
 %% ===================================================================
 
-init([]) ->
-    {ok, #state{}, 0}.
+init([LSock]) ->
+    {ok, #state{lsock = LSock}, 0}.
 
-handle_call(Msg, _From, State) ->
-    {reply, {ok, Msg}, State}.
+handle_call(_Msg, _From, State) ->
+    {reply, ok, State}.
 
 handle_cast(stop, State) ->
     {stop, normal, State}.
 
-handle_info(What, State)->
+handle_info({tcp, Socket, RawData}, State) ->
+    handle_request(Socket, RawData),
+    {stop, normal, State};
+handle_info({tcp_closed, _Socket}, State) ->
+    {stop, normal, State};
+handle_info(timeout, #state{lsock = LSock} = State)->
+    {ok, _Sock} = gen_tcp:accept(LSock),
+    rd_pong_sup:start_child(),
     {noreply, State}.
 
  terminate(_Reason, _State) ->
@@ -44,4 +49,16 @@ handle_info(What, State)->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+%% ===================================================================
+%% Local
+%% ===================================================================
+
+handle_request(Socket, "PING") ->
+    gen_tcp:send(Socket, "PONG");
+handle_request(_Socket, _Request) ->
+    %% Error log an unknown command.
+    ok.
+
+
 
--- src/rd_pong_sup.erl
+++ src/rd_pong_sup.erl
@@ -3,13 +3,13 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/1, start_child/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Args), {I, {I, start_link, [Args]}, permanent, brutal_kill, worker, [I]}).
+-define(CHILD(I, Args), {I, {I, start_link, [Args]}, temporary, brutal_kill, worker, [I]}).
 
 -define(SERVER, ?MODULE).
 
@@ -18,15 +18,17 @@
 %% ===================================================================
 
 %% @doc starts the supervisor.
-start_link() ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+start_link(LSock) ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, [LSock]).
 
+start_child() ->
+    supervisor:start_child(?SERVER, []).
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 %% @private
-init(Arguments) ->
-    RdPongServer = ?CHILD(rd_pong_server, Arguments),
-    {ok, { {one_for_one, 5, 10}, [RdPongServer] } }.
+init([LSock]) ->
+    RdPongServer = ?CHILD(rd_pong_server, [LSock]),
+    {ok, { {simple_one_for_one, 0, 1}, [RdPongServer] } }.
