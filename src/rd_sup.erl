fd0607b19ee2cec4c2c360c9bd421d81b84d0683
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2013-04-22 07:15:48 -0400

    Update supervision tree with new supervisors, including a core supervisor for the app.

--- src/rd_sup.erl
+++ src/rd_sup.erl
@@ -4,15 +4,13 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0, start_child/3, start_child/2, start_host_child/3]).
+-export([start_link/0, start_child/3, start_child/2]).
 
 %% Supervisor callbacks
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, temporary, brutal_kill, Type, [I]}).
--define(CHILD_HOST(I, Type, Args), {I, {I, start_host_link, Args},
-            temporary, brutal_kill, Type, [I]}).
 
 -define(SERVER, ?MODULE).
 
@@ -30,12 +28,6 @@ start_child(Host, Resources, LeaseTime) ->
 
 start_child(Host, LeaseTime) ->
     start_child(Host, [], LeaseTime).
-
-start_host_child(Host, Resources, LeaseTime) ->
-    {StompHost, StompPort, StompUser, StompPassword} = get_stomp_settings(),
-    RdHostServer = ?CHILD_HOST(rd_resource_server, worker,
-        [StompHost, StompPort, StompUser, StompPassword, Resources, Host, LeaseTime]),
-    supervisor:start_child(?SERVER, RdHostServer).
 
 
 %% ===================================================================
