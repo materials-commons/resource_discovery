5596b227c428f636f291e38e6c1a9c56d5ffc6aa
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2013-04-26 16:05:14 -0400

    More debugging of timeout handling. Right now this is my testbed. I need to move this under erlang-stomp-client and add unit tests.

--- src/rd_timeout.erl
+++ src/rd_timeout.erl
@@ -27,11 +27,16 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, #state{lsock = 1}, 0}.
+    gen_stomp:cast(self(), startup),
+    {ok, #state{lsock = 1}}.
 
 handle_call(_Msg, _From, State) ->
     {reply, ok, State}.
 
+handle_cast(startup, State) ->
+    io:format("startup now timeout for 2 seconds~n"),
+    gen_stomp:send("/topic/echo", "Hello", [], 2000),
+    {noreply, State, 2000};
 handle_cast(stop, State) ->
     {stop, normal, State}.
 
@@ -44,7 +49,7 @@ handle_info(bob, State) ->
     {noreply, State};
 handle_info(timeout, #state{lsock = LSock} = State)->
     io:format("rd_timeout timeout~n"),
-    {noreply, State, 20000};
+    {noreply, State, 2000};
 handle_info(Msg, State) ->
     io:format("rd_timeout handle everything: ~p~n", [Msg]),
     {noreply, State}.
