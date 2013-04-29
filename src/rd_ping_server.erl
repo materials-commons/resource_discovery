de971db9aead981ffdcb144466b91b53161910fd
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2013-04-29 11:33:15 -0400

    Cleanup documentation. Review code and identify other cleanup.

--- src/rd_ping_server.erl
+++ src/rd_ping_server.erl
@@ -36,21 +36,31 @@
 
 -define(DEFAULT_HEARTBEAT, (60 * 60)). % Check every hour
 
--record(state, {heartbeat, start_time, port}).
+-record(state, {
+        heartbeat :: integer(), %% How often do we ping known hosts
+        start_time :: integer(), %% Used in timeout determination (heartbeat)
+        port :: port() %% Socket port to connect on.
+    }).
 
 %% ===================================================================
 %% API
 %% ===================================================================
 
+%% @doc start server - uses default heartbeat time
 start_link(Port) ->
     start_link(?DEFAULT_HEARTBEAT, Port).
 
+%% @doc start server
 start_link(Heartbeat, Port) ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [Heartbeat, Port], []).
 
+%% @doc Cause server to ping specified host outside of heartbeat
+-spec ping(string()) -> ok.
 ping(Host) ->
     gen_server:cast(?SERVER, {ping, Host}).
 
+%% @doc Cause server to ping all known hosts outside of heartbeat
+-spec ping_all() -> ok.
 ping_all() ->
     gen_server:cast(?SERVER, ping_all).
 
@@ -58,6 +68,7 @@ ping_all() ->
 %% gen_server callbacks
 %% ===================================================================
 
+%% @private
 init([Heartbeat, Port]) ->
     %% Trap exits and handle ourselves.
     process_flag(trap_exit, true),
@@ -66,12 +77,18 @@ init([Heartbeat, Port]) ->
     State = #state{heartbeat = Heartbeat, start_time = Now, port = Port},
     {ok, State, TimeLeft}.
 
+%% @private
+%%
+%% Handle unknown messages
 handle_call(_Msg, _From, #state{heartbeat = Heartbeat,
                                 start_time = StartTime} = State) ->
     % Error log unknown message
     TimeLeft = lease:time_left(StartTime, Heartbeat),
     {reply, ok, State, TimeLeft}.
 
+%% @private
+%%
+%% Implements the ping(Host) API call.
 handle_cast({ping, Host}, #state{heartbeat = Heartbeat, port = Port,
                                     start_time = StartTime} = State) ->
     case rd_store:lookup(Host) of
@@ -80,6 +97,9 @@ handle_cast({ping, Host}, #state{heartbeat = Heartbeat, port = Port,
     end,
     TimeLeft = lease:time_left(StartTime, Heartbeat),
     {noreply, State, TimeLeft};
+%% @private
+%%
+%% Implements the ping_all() API all.
 handle_cast(ping_all, #state{heartbeat = Heartbeat, port = Port,
                                 start_time = StartTime} = State) ->
     start_pingers(Port),
@@ -88,17 +108,27 @@ handle_cast(ping_all, #state{heartbeat = Heartbeat, port = Port,
 handle_cast(stop, State) ->
     {stop, normal, State}.
 
+%% @private
+%%
+%% Handle process exit trap. Ignore it, we don't really care if the
+%% process worked (Come back and reconsider this statement)
 handle_info({'EXIT', _Pid, _Reason}, State) ->
     {noreply, State};
+
+%% @private
+%%
+%% On timeout we ping all known hosts, then restart heartbeat count down.
 handle_info(timeout, #state{heartbeat = Heartbeat, port = Port} = State)->
     start_pingers(Port),
     Now = lease:seconds_now(),
     NewTimeout = lease:time_left(Now, Heartbeat),
     {noreply, State#state{start_time = Now}, NewTimeout}.
 
- terminate(_Reason, _State) ->
+%% @private
+terminate(_Reason, _State) ->
     ok.
 
+%% @private
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
@@ -106,18 +136,24 @@ code_change(_OldVsn, State, _Extra) ->
 %% Local
 %% ===================================================================
 
+%% Starts a pinger for each known host.
 start_pingers(Port) ->
     lists:foreach(
             fun(Entry) ->
                 start_pinger(Entry, Port)
             end, rd_store:all()).
 
+%% Starts a pinger for the specified host on port. Pid is local process
+%% holding resources for the host.
 start_pinger({Host, Pid}, Port) ->
     start_pinger(Host, Pid, Port).
 
-start_pinger(Host, Pid, Port) ->
+%% Starts a pinger for the specified host on port. Pid is local process
+%% holding resources for the host.start_pinger(Host, Pid, Port) ->
     spawn(?MODULE, ping_host, [Host, Pid, Port]).
 
+%% Function we spawn. Tries to ping host. On anything but success we
+%% shutdown server mapping this.
 ping_host(Host, Pid, Port) ->
     try
         {ok, Sock} = gen_tcp:connect(Host, Port, [{active, false}]),
