158102b4bae4a49a84b1330763e31bd4d6fb1df6
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2013-04-29 07:19:30 -0400

    Handle host startup and shutdown events. There is nothing being stored in the state{} record right now. I have to pass around something, so passing around a blank state record makes it easier to extend in the future.

--- src/rd_new_host_monitor.erl
+++ src/rd_new_host_monitor.erl
@@ -1,12 +1,16 @@
 -module(rd_new_host_monitor).
 -behaviour(gen_stomp).
 
+-include("rd.hrl").
+
 %% API
 -export([start_link/4]).
 
 %% gen_stomp callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
             terminate/2, code_change/3]).
+
+-record(state, {}).
 
 -define(SERVER, ?MODULE).
 
@@ -15,8 +19,9 @@
 %% ===================================================================
 
 start_link(StompHost, StompPort, StompUser, StompPassword) ->
-    gen_stomp:start_link({local, ?SERVER}, ?MODULE,
-                [StompHost, StompPort, StompUser, StompPassword], []).
+    StartupShutdownQueue = ?SS_QUEUE,
+    gen_stomp:start_link({local, ?SERVER}, ?MODULE, StompHost, StompPort,
+            StompUser, StompPassword, [{StartupShutdownQueue, []}], []).
 
 %% ===================================================================
 %% gen_stomp callbacks
@@ -24,11 +29,14 @@ start_link(StompHost, StompPort, StompUser, StompPassword) ->
 
 %% @private
 init([]) ->
-    ok.
+    {ok, #state{}}.
 
 handle_call(_Msg, _From, State) ->
     {reply, ok, State}.
 
+handle_cast([{message, Message}, {queue, _Queue}], State)->
+    handle_message(Message),
+    {noreply, State};
 handle_cast(_Message, State) ->
     {noreply, State}.
 
@@ -40,3 +48,17 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+%% ===================================================================
+%% Local functions
+%% ===================================================================
+
+handle_message([{type, "MESSAGE"}, {header, _Header}, {body, Body}]) ->
+    handle_event(handyterm:string_to_term(Body));
+handle_message(_Message) ->
+    ok.
+
+handle_event(#hostevent{host = Host, event = up}) ->
+    resource_discovery:create(Host);
+handle_event(#hostevent{host = Host, event = down}) ->
+    resource_discovery:delete(Host).
