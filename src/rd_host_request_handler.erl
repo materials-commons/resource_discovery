f61757a9a29377526242e51aa8eeb2678e2662b3
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2013-04-29 10:29:15 -0400

    Documentation and comments.

--- src/rd_host_request_handler.erl
+++ src/rd_host_request_handler.erl
@@ -29,11 +29,15 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 		 terminate/2, code_change/3]).
 
--record(state, {lsock}).
+-record(state, {
+        lsock :: port() % Socket we are listening on.
+    }).
 
 %% ===================================================================
 %% API
 %% ===================================================================
+
+%% @doc Starts server
 start_link(LSock) ->
     gen_server:start_link(?MODULE, [LSock], []).
 
@@ -41,28 +45,37 @@ start_link(LSock) ->
 %% gen_server callbacks
 %% ===================================================================
 
+%% @private
 init([LSock]) ->
     {ok, #state{lsock = LSock}, 0}.
 
+%% @private
 handle_call(_Msg, _From, State) ->
     {reply, ok, State}.
 
+%% @private
 handle_cast(stop, State) ->
     {stop, normal, State}.
 
+%% @private
+%% Handles commands sent over the socket.
 handle_info({tcp, Socket, RawData}, State) ->
     handle_request(Socket, RawData),
     {stop, normal, State};
 handle_info({tcp_closed, _Socket}, State) ->
     {stop, normal, State};
+%% Wait for a connection, start new child and then go on to
+%% handle requests.
 handle_info(timeout, #state{lsock = LSock} = State)->
     {ok, _Sock} = gen_tcp:accept(LSock),
     rd_host_request_sup:start_child(),
     {noreply, State}.
 
+%% @private
 terminate(_Reason, _State) ->
     ok.
 
+%% @private
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
@@ -70,10 +83,12 @@ code_change(_OldVsn, State, _Extra) ->
 %% Local
 %% ===================================================================
 
+%% Send back all the resources for our local host.
 handle_request(Socket, "RESOURCES") ->
     Resources = gen_host_server:fetch(),
     ResourcesAsString = handyterm:term_to_string(Resources),
     gen_tcp:send(Socket, ResourcesAsString);
+%% Unknown command handler.
 handle_request(_Socket, _Request) ->
     %% Error log an unknown command.
     ok.
