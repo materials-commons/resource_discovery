822dc99a8c5ca122d35e060f511eba239fc2dc98
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2013-04-29 11:45:37 -0400

    Add documentation and specs.

--- src/rd_resource_db.erl
+++ src/rd_resource_db.erl
@@ -22,30 +22,48 @@
 
 -include("resource.hrl").
 
+-type descriptor() :: ets:tid().
+
 % API
 -export([new/0, insert/2, delete_by_name/2, delete_by_type/2,
             delete_all/1, find_by_name/2, find_by_type/2, all/1]).
 
-new() ->
+%% @doc Initializes database
+-spec new() -> descriptor().
+ new() ->
     ets:new(resources, [private, bag, {keypos, #resource.name}]).
 
+%% @doc Inserts a new resource into tabl
+-spec insert(descriptor(), resource()) -> true.
 insert(Descriptor, Resource) ->
 	true = ets:insert(Descriptor, Resource).
 
+%% @doc Delete entry by resource name.
+-spec delete_by_name(descriptor(), any()) -> true.
 delete_by_name(Descriptor, Name) ->
     ets:match_delete(Descriptor, #resource{name = Name, _ = '_'}).
 
+%% @doc Delete entry by resource type.
+-spec delete_by_type(descriptor(), any()) -> true.
 delete_by_type(Descriptor, Type) ->
     ets:match_delete(Descriptor, #resource{type = Type, _ = '_'}).
 
+%% @doc Delete all entries.
+-spec delete_all(descriptor()) -> true.
 delete_all(Descriptor) ->
     ets:delete_all_objects(Descriptor).
 
+%% @doc Finds resources matching resource name
+-spec find_by_name(descriptor(), any()) -> [resource()] | [].
 find_by_name(Descriptor, Name) ->
     ets:match_object(Descriptor, #resource{name = Name, _ = '_'}).
 
+%% @doc Finds resources matching resource type
+-spec find_by_type(descriptor(), any()) -> [resource()] | [].
 find_by_type(Descriptor, Type) ->
     ets:match_object(Descriptor, #resource{type = Type, _ = '_'}).
 
+%% @doc Returns all known resource entries.
+-spec all(descriptor()) -> [resource()] | [].
 all(Descriptor) ->
     ets:match_object(Descriptor, #resource{_ = '_'}).
