c66de9fed5967edf56fa276860e43d018bac17b3
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2013-05-01 10:33:38 -0400

    Remove STOMP and put rd_host_request calls in place. Flesh out the calls supported.

--- src/rd_host_server.erl
+++ src/rd_host_server.erl
@@ -25,8 +25,8 @@
 -include_lib("handyman/include/jsonerl.hrl").
 
 %% API
--export([start_link/6, fetch/0, delete_resource/1, add_resource/1,
-            stop/0, stop/1]).
+-export([start_link/6, fetch/0, delete_resources/1, add_resource/1,
+            update_resources/1, stop/0, stop/1]).
 
 %% gen_stomp callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -40,8 +40,6 @@
     {
         rd :: rd_resource_db:descriptor(), % descriptor to resource_db
         host :: string(), % Our host
-        command_queue :: string(), % Command queue to listen for requests on.
-        broadcast_queue :: string(), % Queue to broadcast changes on.
         ss_queue :: string() %% Startup/Shutdown queue.
     }).
 
@@ -51,13 +49,10 @@
 
 %% @doc starts the main server that holds the real view of the resources
 start_link(StompHost, Port, Username, Password, ResourceHost, Resources) ->
-    HostBroadcastTopic = string:concat("/topic/rd_", ResourceHost),
-    HostCommandQueue = string:concat("/queue/rd_command_", ResourceHost),
     ResourceRd = #resource{host = ResourceHost, name = "resource_discovery",
                             type = "resource_discovery"},
-    gen_stomp:start_link({local, ?SERVER}, ?MODULE, StompHost, Port, Username, Password,
-        [{HostCommandQueue, []}],
-        [HostBroadcastTopic, HostCommandQueue, ResourceHost, [ResourceRd | Resources]]).
+    gen_stomp:start_link({local, ?SERVER}, ?MODULE, StompHost, Port,
+        Username, Password, [], [ResourceHost, [ResourceRd | Resources]]).
 
 %% @doc Fetch resources for this server.
 -spec fetch() -> [resource()].
@@ -75,9 +70,14 @@ stop(Pid) ->
     gen_stomp:cast(Pid, stop).
 
 %% @doc Delete a resource.
--spec delete_resource(resource()) -> ok.
-delete_resource(#resource{} = Resource) ->
-    gen_stomp:cast(?SERVER, {delete_resource, Resource}).
+-spec delete_resources([resource()]) -> ok.
+delete_resources(Resources) ->
+    gen_stomp:cast(?SERVER, {delete_resources, Resources}).
+
+%% @doc Update a resource
+-spec update_resources([resource()]) -> ok.
+update_resources(Resources) ->
+    gen_stomp:cast(?SERVER, {update_resources, Resources}).
 
 %% @doc Add a resource.
 -spec add_resource(resource()) -> ok.
@@ -89,17 +89,14 @@ add_resource(#resource{} = Resource) ->
 %% ===================================================================
 
 %% @doc No resources specified, need to query host for them.
-init([HostBroadcastTopic, HostCommandQueue, ResourceHost, Resources]) ->
+init([ResourceHost, Resources]) ->
     Rd = rd_resource_db:new(),
 
     %% Do initialization outside of init.
     gen_stomp:cast(self(), {startup, Resources}),
     rd_store:insert(ResourceHost, self()),
 
-    State = #state{ command_queue = HostCommandQueue, host = ResourceHost,
-                    broadcast_queue = HostBroadcastTopic, rd = Rd,
-                    ss_queue = ?SS_QUEUE},
-
+    State = #state{ host = ResourceHost, rd = Rd,ss_queue = ?SS_QUEUE},
     {ok, State}.
 
 %% @doc Retrieve all resources
@@ -107,33 +104,39 @@ handle_call(fetch, _From, #state{rd = Rd} = State) ->
     Resources = rd_resource_db:all(Rd),
     {reply, {ok, Resources}, State}.
 
-handle_cast([{message, Message}, {queue, Queue}],
-        #state{broadcast_queue = BroadcastQueue, rd = Rd} = State) ->
-    handle_message(Rd, Message, Queue, BroadcastQueue),
-    {noreply, State};
-
-handle_cast({add_resource, Resource},
-        #state{rd = Rd, broadcast_queue = BroadcastQueue} = State) ->
-    add_resources(Rd, [Resource]),
-    broadcast_resource(Resource, BroadcastQueue),
+handle_cast({add_resource, Resource}, #state{rd = Rd, host = MyHost} = State) ->
+    rd_resource_db:add_resources(Rd, [Resource]),
+    broadcast_to_remotes(MyHost, [Resource], send_resources),
     {noreply, State};
 
 %% @doc Add resources that were specified in startup.
 handle_cast({startup, Resources},
                 #state{rd = Rd, ss_queue = SSQueue, host = Host} = State) ->
-    add_resources(Rd, Resources),
+    rd_resource_db:add_resources(Rd, Resources),
     UpEvent = handyterm:term_to_string(#hostevent{host = Host, event = up}),
+
     %% Wait before broadcasting our resources
     TimeToWait = handyconfig:get_env_default(resource_discovery, rd_host_wait, ?DEFAULT_WAIT),
     gen_stomp:send(SSQueue, UpEvent, [], TimeToWait),
     {noreply, State, TimeToWait};
 
+handle_cast({update_resources, Resources}, #state{rd = Rd, host = MyHost} = State) ->
+    rd_resource_db:update_resources(Rd, Resources),
+    broadcast_to_remotes(MyHost, Resources, update_resources),
+    {noreply, State};
+
+handle_cast({delete_resources, Resources}, #state{rd = Rd, host = MyHost} = State) ->
+    rd_resource_db:delete_resources(Rd, Resources),
+    broadcast_to_remotes(MyHost, Resources, delete_resources),
+    {noreply, State};
+
 %% TimeToWait Stop the server.
 handle_cast(stop, State) ->
     {stop, normal, State}.
 
-handle_info(timeout, #state{broadcast_queue = BroadcastQueue, rd = Rd} = State) ->
-    broadcast_resources(BroadcastQueue, Rd),
+handle_info(timeout, #state{rd = Rd, host = MyHost} = State) ->
+    Resources = rd_resource_db:all(Rd),
+    broadcast_to_remotes(MyHost, Resources, send_resources),
     {noreply, State};
 
 %% @doc On timeout go out and query for the resources.
@@ -142,7 +145,7 @@ handle_info(_Message, State) ->
     {noreply, State}.
 
 terminate(_Reason, #state{ss_queue = SSQueue, host = Host}) ->
-    % Remove mapping for process
+    % Remove mapping for process and tell everyone we are going down!
     DownEvent = handyterm:term_to_string(#hostevent{host = Host, event = down}),
     gen_stomp:send(SSQueue, DownEvent, []),
     rd_store:delete_by_pid(self()),
@@ -155,40 +158,14 @@ code_change(_OldVsn, State, _Extra) ->
 %% Local functions
 %% ===================================================================
 
-%% @doc handle messages on the message queues
-handle_message(Rd, [{type, "MESSAGE"},
-                    {header, _Header}, {body, Body}],
-                _Queue, BroadcastQueue) ->
-    handle_commands(Body, BroadcastQueue, Rd);
-
-%% @doc Handle non-message types
-handle_message(_Rd, _Message, _Queue, _BroadcastQueue) ->
-    ok.
-
-%% @doc Add a list of resources to our database of resources
-add_resources(_Rd, []) ->
+broadcast_to_remotes(_MyHost, [], _Method) ->
     ok;
-add_resources(Rd, [R|T]) ->
-    rd_resource_db:insert(Rd, R),
-    add_resources(Rd, T).
-
-%% @doc Handle different types of commands.
-handle_commands("RESOURCES", BroadcastQueue, Rd) ->
-    broadcast_resources(BroadcastQueue, Rd),
-    ok;
-handle_commands(Command, Queue, _Rd) ->
-    % Add error logging here.
-    io:format("handle_commands fall through ~p:~p~n", [Command, Queue]),
-    ok.
-
-%% @doc Send each resource one at a time.
-broadcast_resources(BroadcastQueue, Rd) ->
-    Resources = rd_resource_db:all(Rd),
-    ResourcesAsString = handyterm:term_to_string(Resources),
-    gen_stomp:send(BroadcastQueue, ResourcesAsString, []).
-
-%% @doc Transform resource to list of Records, turn into string and send
-broadcast_resource(Resource, BroadcastQueue) ->
-    %% Other side expects a list of records.
-    RecordsListAsString = handyterm:term_to_string([Resource]),
-    gen_stomp:send(BroadcastQueue, RecordsListAsString, []).
+broadcast_to_remotes(MyHost, Resources, Method) ->
+    lists:foreach(
+        fun({Host, _Pid}) ->
+            case Host =:= MyHost of
+                true -> ok;
+                false ->
+                    spawn(rd_host_request, Method, [Host, MyHost, Resources])
+            end
+        end, rd_store:all()).
